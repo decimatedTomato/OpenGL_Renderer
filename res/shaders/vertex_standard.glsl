3d215653fec02d863f4355f672e50b1d4fd26cf2
decimatedTomato <greentomatopower@gmail.com>
2024-07-20 00:17:09 +0200

    Yeap that fixed it

--- res/shaders/vertex_standard.glsl
+++ res/shaders/vertex_standard.glsl
@@ -1,15 +1,15 @@
 #version 330 core
 precision highp float;
 
-attribute vec3 aPosition;
-attribute vec4 aVertexColor;
-attribute vec2 aUV;
+layout(location = 0) in vec3 aPosition;
+layout(location = 1) in vec4 aVertexColor;
+layout(location = 2) in vec2 aUV;
 
 //uniform mat4 uModelViewMatrix;
 //uniform mat4 uProjectionMatrix;
 
-varying vec2 vUV;
-varying vec4 vVertexColor;
+out vec2 vUV;
+out vec4 vVertexColor;
 
 void main() {
 	// Apply the camera transform
@@ -18,7 +18,7 @@ void main() {
 	//  gl_Position = uProjectionMatrix * viewModelPosition;
 
 	// Pass data to fragment shader
-	vUV = aUV;
 	vVertexColor = aVertexColor;
+	vUV = aUV;
 	gl_Position = vec4(aPosition, 1.0);
 }
