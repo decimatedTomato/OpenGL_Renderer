fc2294a37c3cf973d0dbc70e7dc7433d2fbdf96b
decimatedTomato <greentomatopower@gmail.com>
2024-07-23 17:15:23 +0200

    [impl] Made a more exciting default shader

--- res/shaders/fragment_basic.glsl
+++ res/shaders/fragment_basic.glsl
@@ -7,9 +7,9 @@ in vec2 vUV;
 uniform float uTime;
 
 void main() {
-//	vec3 colors = vUV.x * vec3(0.5, 0.0, 0) + vUV.y * vec3(0, 0.5, 0.5);
-//	colors += sin(uTime * vec3(1, 2, 4) + distance(vUV, vec2(0.5))) / 2.0;
-	vec3 colors = vec3(vUV.xy, 0);
+	vec3 colors = vUV.x * vec3(0.5, 0.0, 0) + vUV.y * vec3(0, 0.5, 0.5);
+	colors += sin(uTime * vec3(1, 2, 4) + distance(vUV, vec2(0.5))) / 2.0;
+//	vec3 colors = vec3(vUV.xy, 0);
 //	vec3 colors = vVertexColor.rgb;
 	gl_FragColor = vec4(colors, 1.0);
 }
